6459104c1f0b022877dd8ac9cc57b92092d59705
Marcus <marcusl1@hotmail.com>
2024-02-25 14:34:23 +0100

    add more tests får RSS

--- src/Tests/ElementTests.cs
+++ src/Tests/ElementTests.cs
@@ -186,6 +186,73 @@ namespace Tests
 
         }
 
+        [Fact]
+        public void TestParseRss()
+        {
+            string path = @"I:\Temp\19\andrewlock.net\145884EB4AF604D110E533BE6012508517BAB58F.xml";
+            XmlReader xmlReader = new XmlReader();
+            xmlReader.Parse(File.ReadAllText(path));
+
+            foreach (Element item in xmlReader.Query("item"))
+            {
+
+                List<Element> links = item.Query("link");
+
+                Assert.True(item.Query("title").Count > 0);
+                Assert.NotEmpty(item.Query("title").First().Value);
+
+            }
+        }
+
+        [Fact]
+        public void TestCData()
+        {
+            string xml = """
+                <item>
+                    <Title type="markdown" count="50"><![CDATA[<h1>test</h1>]]></Title>
+                </item>
+                """;
+
+            XmlReader xmlReader = new XmlReader();
+            xmlReader.Parse(xml);
+
+            Element root = xmlReader.Query("item Title").First();
+
+            Assert.Equal("<h1>test</h1>", root.Value);
+        }
+
+        [Fact]
+        public void TestRssItem()
+        {
+            string xml = """
+                <channel>
+                    <title><![CDATA[Andrew Lock | .NET Escapades]]></title>
+                    <description><![CDATA[Hi, my name is Andrew, or ‘Sock’ to most people. This blog is where I share my experiences as I journey into ASP.NET Core.]]></description>
+                    <link>https://andrewlock.net/</link>
+                    <generator>andrewlock-blog-engine</generator>
+                    <lastBuildDate>Tue, 13 Feb 2024 10:15:41 GMT</lastBuildDate>
+                    <atom:link href="https://andrewlock.net/rss/" rel="self" type="application/rss+xml"/>
+                    <ttl>60</ttl>
+                    <item>
+                	    <title><![CDATA[8 ways to set the URLs for an ASP.NET Core app]]></title>
+                	    <description><![CDATA[In this post I describe 8 different ways to set which URLs your ASP.NET Core application listens on.]]></description>
+                	    <link>https://andrewlock.net/8-ways-to-set-the-urls-for-an-aspnetcore-app/</link>
+                	    <guid isPermaLink="true">https://andrewlock.net/8-ways-to-set-the-urls-for-an-aspnetcore-app/</guid>
+                	    <pubDate>Tue, 13 Feb 2024 10:00:00 GMT</pubDate>
+                	    <dc:creator><![CDATA[Andrew Lock]]></dc:creator>
+                    </item>
+                </channel>
+                """;
+
+
+            XmlReader xmlReader = new XmlReader();
+            xmlReader.Parse(xml);
+
+            //Assert.NotEmpty(xmlReader.Query("channel title").First().Value);
+            Assert.NotEmpty(xmlReader.Query("channel item title").First().Value);
+
+        }
+
 
 
     }
