f106ee95e936012d470d3a4735b5e493c01b6c66
Marcus <marcusl1@hotmail.com>
2024-10-19 22:56:32 +0200

    make new version

--- src/Tests/ParsingTests.cs
+++ src/Tests/ParsingTests.cs
@@ -1,14 +1,13 @@
 using XmlQuery;
-using XmlQuery.Parsing;
 namespace Tests
 {
     public class ParsingTests
     {
         [Fact]
         public void ParsingBooks()
-        {      
+        {
             XmlReader xmlReader = new XmlReader();
-            xmlReader.Parse(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "data","books.xml")));
+            xmlReader.Parse(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "data", "books.xml")));
 
             Assert.NotNull(xmlReader.Document);
         }
@@ -29,6 +28,25 @@ namespace Tests
             xmlReader.Parse(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "data", "tvshow.nfo")));
 
             Assert.NotNull(xmlReader.Document);
+
+            foreach (XmlQuery.Xml.Element tvshow in xmlReader.Query("tvshow"))
+            {
+                string title = tvshow.QueryValue("title");
+                string showtitle = tvshow.QueryValue("showtitle");
+                ushort year = Convert.ToUInt16(tvshow.QueryValue("year"));
+                decimal rating = Convert.ToDecimal(tvshow.QueryValue("rating"));
+                decimal userrating = Convert.ToDecimal(tvshow.QueryValue("userrating"));
+                ushort votes = Convert.ToUInt16(tvshow.QueryValue("votes"));
+                string plot = tvshow.QueryValue("plot");
+                byte runtime = Convert.ToByte(tvshow.QueryValue("runtime"));
+                uint id = Convert.ToUInt32(tvshow.QueryValue("id"));
+                string imdbid = tvshow.QueryValue("imdbid");
+                DateTime premiered = Convert.ToDateTime(tvshow.QueryValue("premiered"));
+                string status = tvshow.QueryValue("status");
+                bool watched = Convert.ToBoolean(tvshow.QueryValue("watched"));
+                string playcount = tvshow.QueryValue("playcount");
+                string dateadded = tvshow.QueryValue("dateadded");
+            }
         }
     }
 }
